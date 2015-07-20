584908bf00f465c5d84eece07de85162b0f2e5c6
Daniel Borges Ferreira da Silva <daniel_borges_93@yahoo.com.br>
2015-07-20 16:33:29 -0300

    Segundo commit.

--- src/memcached/Memcached.java
+++ src/memcached/Memcached.java
@@ -5,6 +5,10 @@
  */
 package memcached;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.Future;
+import net.spy.memcached.MemcachedClient;
+
 /**
  *
  * @author danielborges93
@@ -16,8 +20,28 @@ public class Memcached {
      */
     public static void main(String[] args) {
 	
-	
-	
+	try {
+	    // Connecting to Memcached server on localhost
+	    MemcachedClient mcc = new MemcachedClient(new InetSocketAddress("192.168.25.89", 11211));
+	    System.out.println("Connection to server sucessful.");
+
+	    Future fo = mcc.set("tutorialspoint", 900, "Free Education");
+
+	    // print status of set method
+	    System.out.println("set status:" + fo.get());
+
+	    // retrieve and check the value from cache
+	    System.out.println("tutorialspoint value in cache - " + mcc.get("tutorialspoint"));
+
+	    System.out.println("a = " + mcc.get("a"));
+
+	    // Shutdowns the memcached client
+	    mcc.shutdown();
+
+	} catch (Exception ex) {
+	    System.out.println(ex.getMessage());
+	}
+
     }
-    
+
 }
